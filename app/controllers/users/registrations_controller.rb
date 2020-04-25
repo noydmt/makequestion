dc34d5f6533a7d34e57bffe690422cd618358e4f
NaoyaTsudome <noy.dmt1021@gmail.com>
2020-04-26 03:08:51 +0900

    override sign_up method

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,25 +10,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #end
 
   # POST /resource
-  def create
-    build_resource(sign_up_params)
-    resource.save
-    yield resource if block_given?
-    if resource.persisted?
-      if resource.active_for_authentication?
-        set_flash_message! :notice, :signed_up
-        respond_with resource, location: after_sign_up_path_for(resource)
-      else
-        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
-        expire_data_after_sign_in!
-        respond_with resource, location: after_inactive_sign_up_path_for(resource)
-      end
-    else
-      clean_up_passwords resource
-      set_minimum_password_length
-      respond_with resource
-    end
-  end
+  #def create
+  #  super  
+  #end
   
 
   # GET /resource/edit
@@ -75,4 +59,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  def sign_up(resource_name, resource)
+    if current_user
+      sign_in(resource_name, resource)
+    end
+  end
 end
