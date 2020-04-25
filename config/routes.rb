45653329fb39f8450e0b8d9f76e62617ac0c864a
NaoyaTsudome <noy.dmt1021@gmail.com>
2020-04-25 22:35:00 +0900

    return route

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  devise_for :users, skip: :all
+  #devise_for :users, controllers: {
+  #  registrations: 'users/registrations',
+  #  sessions: 'users/sessions' 
+  #}
 
-  devise_scope :user do
-    root to: "users/sessions#new", as: :new_user_session
-    get "sign_up", :to => "users/registrations#new", as: :new_user_registration
-    post "sign_up", :to => "users/registrations#create", as: :user_registration
-    get "sign_out", :to => "users/sessions#destroy", as: :destroy_user_session
-    get "password/new", :to => "users/password#new", as: :new_user_password
-  end
-
+  devise_for :users
 end
