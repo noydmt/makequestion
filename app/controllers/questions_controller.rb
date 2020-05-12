e7d761fa38a23a39e61be9ae38d7a9ef07d575a6
NaoyaTsudome <noy.dmt1021@gmail.com>
2020-05-12 23:21:02 +0900

    forgot commit

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,41 @@
 class QuestionsController < ApplicationController
-before_action :authenticate_user!
+  before_action :authenticate_user!
+
   def index
+    @questions = current_user.questions.all
+  end
+
+  def new
+    @questions = current_user.questions.all
+    @question = Question.new
+  end
+
+  def create
+    @question = current_user.questions.new(question_params)
+    if @question.save
+      redirect_to new_question_path, success: "質問を作成しました"
+    else
+      @questions = current_user.questions.all
+      render :new
+    end
+  end
+
+  def show
+
+  end
+
+  def destroy
+    question = Question.find_by(id: params[:id])
+    if question.destroy
+      redirect_to questions_path, success: "質問を削除しました"
+    else
+      flash.now[:alert] = "削除に失敗しました"
+      render :index
+    end
+  end
+
+  private 
+  def question_params
+    params.require(:question).permit(:content)
   end
 end
