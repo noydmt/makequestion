2f86012beafbaf55a82fb0b6c11a5cebfe5dfe5f
NaoyaTsudome <noy.dmt1021@gmail.com>
2020-05-12 23:13:28 +0900

    display,create,destroy questions function

---
+++ app/models/question.rb
@@ -0,0 +1,5 @@
+class Question < ApplicationRecord
+  validates :content, presence: true, length: { in: 2..50 }
+  validates :user_id, presence: true
+  belongs_to :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_01_162854) do
+ActiveRecord::Schema.define(version: 2020_05_10_164949) do
+
+  create_table "questions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_questions_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +37,5 @@ ActiveRecord::Schema.define(version: 2020_05_01_162854) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "questions", "users"
 end
